56daa678ca90b6f6e51acd7fe70069627e56aeb3
Toni Lamont <tlam212@gmail.com>
2019-05-03 14:28:37 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def dictionary
+  "hello": "hi"
+  when "hello"
+    puts "hi"
+  when "to" || "two" || "too"
+    puts "2"
+  when "for" || "four" 
+    puts "4"
+  when "be"
+    puts "b"
+  when "you"
+    puts "u"
+  when "at"
+    puts "@"
+  when "and"
+    puts "&"
+  end
+end
+ 
+def words_substituter(string)
+  
+end
+    
